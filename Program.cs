f9fd337e04c1368000e1516665e41d840eb7f643
Alexey <leha.basanov123@gmail.com>
2023-03-11 14:21:23 +0300

    Third task

--- Program.cs
+++ Program.cs
@@ -40,3 +40,24 @@ else
     Console.WriteLine($"У числа {number} третья цифра - {thirdNum(number)}");
 }
 */
+
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+bool weekend(int num)
+{
+    if(num ==6 || num == 7)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+Console.WriteLine("Введите номер дня недели от 1 до 7");
+
+int day = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"День номер {day} является выходным - {weekend(day)}");
